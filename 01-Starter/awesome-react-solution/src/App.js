de2cf80a5b494eb744050d78286298f2615ac565
Jonas Bandi <dev@jonasbandi.net>
2017-11-27 15:02:39 +0100

    Exercise 2: demo of more complex state

--- 01-Starter/awesome-react-solution/src/App.js
+++ 01-Starter/awesome-react-solution/src/App.js
@@ -14,6 +14,8 @@ class App extends Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <Greeter/>
+                <Greeter/>
+                <Greeter/>
                 <p className="App-intro">
 
                     To get started, edit <code>src/App.js</code> and save to reload.
--- 01-Starter/awesome-react-solution/src/Greeter.js
+++ 01-Starter/awesome-react-solution/src/Greeter.js
@@ -2,26 +2,51 @@ import React, {Component} from 'react';
 
 class Greeter extends Component {
 
-    constructor() {
+    state = {
+        transient: {
+            name: ''
+        },
+        committed: {
+            name: 'World'
+        }
+    };
+
+    constructor(){
         super();
-        this.state = {name: 'World'};
+        this.commitChange = this.commitChange.bind(this);
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <input type="text" onChange={(e) => this.handleChange(e)}/>
+                    <input value={this.state.transient.name}
+                           type="text"
+                           onChange={(e) => this.handleChange(e)}
+                    />
+                    <button onClick={this.commitChange}>
+                        Update!
+                    </button>
+
                 </div>
                 <p>
-                    Hello {this.state.name}
+                    Hello {this.state.committed.name}
                 </p>
             </div>
         );
     }
 
     handleChange(e) {
-        this.setState({name: e.target.value})
+        const name = e.target.value;
+        const newTransient = {...this.state.transient, name};
+        this.setState({transient: newTransient, number: 42})
+    }
+
+    commitChange() {
+        // const newCommitted = {...this.state.transient};
+
+        this.setState({committed: this.state.transient})
+
     }
 }
 
