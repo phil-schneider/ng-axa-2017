fd9a590d14ca3fca7b6de3d2a1a68fe7601f9274
Philipp <github-commit@phil-schneider.de>
2017-11-28 14:55:19 +0100

    Dienstag mittag

---
+++ 10-ToDoApp/00-todo-skeleton/src/DoneList.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import NewToDo from './NewToDo';
+import ToDoList from './ToDoList';
+
+export default class PendingList extends Component {
+
+
+    render() {
+        return (
+
+            <section className="todoapp">
+
+                <div className="main">
+                    <ToDoList items={this.props.items}
+                              removeListItemFunc={this.props.removeListItemFunc}
+                              filterItemFunc={(i) => (i.done === true)}
+                    />
+
+                </div>
+            </section>
+        )
+    }
+}
---
+++ 10-ToDoApp/00-todo-skeleton/src/PendingList.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import NewToDo from './NewToDo';
+import ToDoList from './ToDoList';
+
+export default class PendingList extends Component {
+
+    render() {
+        return (
+            <section className="todoapp">
+                <NewToDo addListItemFunc={this.props.addListItemFunc}/>
+
+                <div className="main">
+                    <ToDoList items={this.props.items}
+                              removeListItemFunc={this.props.removeListItemFunc}
+                              filterItemFunc={(i) => (i.done === false)}
+                    />
+                </div>
+            </section>
+        )
+    }
+}
